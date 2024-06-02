f642a588759b874c8ca03d6532af2595e716d240
Kamran-Muradov <kamranshmu@code.edu.az>
2024-06-03 00:22:44 +0400

    Frontent-Project-MVC

---
+++ Educal-MVC/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Educal_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Educal_MVC.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<CourseImage> CourseImages { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) { }
+    }
+}
